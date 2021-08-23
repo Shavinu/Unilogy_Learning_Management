82c124178e3ffd8e20f6cc72c6d509bb03b88b93
Shavinu Amarasinghe <89382832+Shavinu@users.noreply.github.com>
2021-08-23 14:40:49 +0530

    Add files via upload

---
+++ Learning Management System/src/MySqlConnection.java
@@ -0,0 +1,4 @@
+public class MySqlConnection {
+    public String Connection = "jdbc:mysql://localhost/unilogy?verifyServerCertificate=false&useSSL=true";
+    public String MyDriver = "com.mysql.jdbc.Driver";
+}
